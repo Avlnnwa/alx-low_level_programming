b7b02fa4e17fecaf780d7bcb3eecfcb2540920e6
Avlnnwa <adukwuvictor15@gmail.com>
2023-07-20 08:52:14 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print line
+ * @n: prints straight line
+ * Return: no return
+ */
+
+void print_diagonal(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
