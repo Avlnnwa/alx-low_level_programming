0654d73b247668533e2a63e0562b20504cd4a6e2
Avlnnwa <adukwuvictor15@gmail.com>
2023-07-20 08:25:15 -0700

    task 4 edit 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,17 @@
  * (neither 2 nor 4).
  * Return: no return.
  */
-
 void print_most_numbers(void)
+
 {
+	int ch;
 
-int ch;
+	for (ch = 48; ch < 58; ch++)
 
-for (ch >= 48; ch <= 57; ch++)
-{
+	{
+		if (ch != 50 && ch != 52)
 
-if (ch != 50 && ch != 52)
-_putchar(ch);
-}
-_putchar('\n');
+			_putchar(ch);
+	}
+	_putchar('\n');
 }
