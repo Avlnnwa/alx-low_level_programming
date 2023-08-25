c08e3661223d77652fc171ab4f10a84647945b06
Avlnnwa <adukwuvictor15@gmail.com>
2023-08-25 13:32:12 -0700

    final task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,17 +5,24 @@
  * @h: Pointer to the head of the linked list.
  * Return: The number of nodes in the linked list.
  */
-size_t print_list(const list_t *h) {
-    if (h == NULL) {
-        return 0;
-    }
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
 
-    if (h->str == NULL) {
-        printf("[0] (nil)\n");
-    } else {
-        printf("[%u] %s\n", h->len, h->str);
-    }
+while (h != NULL)
+{
 
-    return (1 + print_list(h->next));
+if (h->str == NULL)
+{
+printf("[0] (nil)\n");
 }
+else
+{
+printf("[%u] %s\n", h->len, h->str);
+}
+h = h->next;
+count++;
 
+}
+return (count);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,16 +8,17 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string with malloc
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
-
-typedef struct lists
+typedef struct list_s
 {
-	char *str;
-	unsigned int length;
-	struct lists *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
