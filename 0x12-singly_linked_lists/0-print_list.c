acbcfc006ab85b0b97e7223aa24aee79e3b677d1
Avlnnwa <adukwuvictor15@gmail.com>
2023-08-25 13:09:06 -0700

    task 0 try again

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,17 +7,17 @@
 
 size_t print_list(const list_t *h)
 {
-size_t count;
+	size_t nelem;
 
-count = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%d] %s\n", 0, "(nil)");
-else
-printf("[%d] %s\n", h->length, h->str);
-h = h->next;
-count++;
-}
-return (count);
+	nelem = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nelem++;
+	}
+	return (nelem);
 }
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,24 +1,24 @@
-#ifndef LISTS
-#define LISTS
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * struct lists - singly linked list
- * @str: string - malloc
- * @length: length of the string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct lists
+typedef struct list_s
 {
 	char *str;
-	unsigned int length;
-	struct lists *next;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
