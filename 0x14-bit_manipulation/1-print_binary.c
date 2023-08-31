fb62e812a92fd47e343169cbe1e60852f00110f9
Avlnnwa <adukwuvictor15@gmail.com>
2023-08-31 12:09:10 -0700

    task 1-print

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,26 +8,17 @@
 
 void print_binary(unsigned long int n)
 {
-unsigned long int a = 1UL << (sizeof(unsigned long int) * 8 - 1);
-
-int print_binary = 0;
-
-while (a)
-{
-if (n & a)
-{
-_putchar('1');
-print_binary = 1;
-}
-
-else if (print_binary)
-{
-_putchar('0');
-}
-a >>= 1;
-}
-
-if (!print_binary)
-	_putchar('0');
-
+	if (n >> 0)
+	{
+		if (n >> 1)
+		{
+			print_binary(n >> 1);
+			
+		}
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
+	}
 }
