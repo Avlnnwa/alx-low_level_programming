89d0e59d2e0a3c028d9acbe87ac44586f2663213
Avlnnwa <adukwuvictor15@gmail.com>
2023-08-31 11:55:54 -0700

    updated task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,13 +3,31 @@
 /**
  * print_binary - Prints the binary representation of a number
  * @n: The number to be printed in binary
+ * Return: no return
  */
 
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+unsigned long int a = 1UL << (sizeof(unsigned long int) * 8 - 1);
 
-	putchar((n & 1) + '0');
+int print_binary = 0;
+
+while (a)
+{
+if (n & a)
+{
+_putchar('1');
+print_binary = 1;
+}
+
+else if (print_binary)
+{
+_putchar('0');
+}
+a >>= 1;
+}
+
+if (!print_binary)
+	_putchar('0');
 
 }
