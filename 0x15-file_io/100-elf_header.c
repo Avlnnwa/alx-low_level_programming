157443a171a79d3d208d0e6ab482b78f65f00b22
Avlnnwa <adukwuvictor15@gmail.com>
2023-09-05 15:35:33 -0700

    updated task 100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -9,7 +9,31 @@
  * @header: Pointer to the ELF header structure.
  */
 
-void print_elf_info(Elf64_Ehdr *header);
+void print_elf_header(Elf64_Ehdr *header)
+{
+printf("Magic:   ");
+for (int i = 0; i < EI_NIDENT; i++)
+printf("%02x ", header->e_ident[i]);
+printf("\n");
+
+printf("Class:   %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ?
+		"ELF64" : "ELF32");
+printf("Data:    %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
+		"2's complement, little endian" : "Unknown");
+printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
+printf("OS/ABI:  %d\n", header->e_ident[EI_OSABI]);
+printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
+printf("Type:    %d\n", header->e_type);
+printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
+}
+
+/**
+ * main - Display ELF header information from an ELF file.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
+ * Return: 0 on success, 98 on error.
+ */
+
 int main(int argc, char *argv[])
 {
 int fd;
@@ -17,45 +41,31 @@ Elf64_Ehdr header;
 
 if (argc != 2)
 {
-	dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
-	return (98);
+dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]);
+return (98);
 }
+
 fd = open(argv[1], O_RDONLY);
 if (fd == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Cannot open file %s\n", argv[1]);
-	return (98);
+dprintf(STDERR_FILENO, "Error: Cannot open file %s\n", argv[1]);
+return (98);
 }
+
 if (read(fd, &header, sizeof(header)) != sizeof(header) ||
-		header.e_ident[EI_MAG0] != ELFMAG0 ||
-		header.e_ident[EI_MAG1] != ELFMAG1 ||
-		header.e_ident[EI_MAG2] != ELFMAG2 ||
-		header.e_ident[EI_MAG3] != ELFMAG3)
+header.e_ident[EI_MAG0] != ELFMAG0 ||
+header.e_ident[EI_MAG1] != ELFMAG1 ||
+header.e_ident[EI_MAG2] != ELFMAG2 ||
+header.e_ident[EI_MAG3] != ELFMAG3)
+
 {
-	dprintf(STDERR_FILENO, "Error: Not an ELF file: %s\n", argv[1]);
-	close(fd);
-	return (98);
+dprintf(STDERR_FILENO, "Error: Not an ELF file: %s\n", argv[1]);
+close(fd);
+return (98);
 }
-print_elf_info(&header);
+
+print_elf_header(&header);
+
 close(fd);
 return (0);
-
-}
-
-void print_elf_info(Elf64_Ehdr *header)
-{
-	printf("Magic:   ");
-	for (int i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", header->e_ident[i]);
-	printf("\n");
-
-	printf("Class:   %s\n", header->e_ident[EI_CLASS] == ELFCLASS64 ?
-			"ELF64" : "ELF32");
-	printf("Data:    %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ?
-			"2's complement, little endian" : "Unknown");
-	printf("Version: %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("OS/ABI:  %d\n", header->e_ident[EI_OSABI]);
-	printf("ABI Version: %d\n", header->e_ident[EI_ABIVERSION]);
-	printf("Type:    %d\n", header->e_type);
-	printf("Entry point address: 0x%lx\n", (unsigned long)header->e_entry);
 }
