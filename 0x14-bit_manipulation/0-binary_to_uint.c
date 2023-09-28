1280b37a44bdad0165ed3e688039f05afd518075
Avlnnwa <adukwuvictor15@gmail.com>
2023-09-28 15:40:29 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,28 @@
 
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int a = 0;
+	unsigned int i;
+	int len;
+	int base;
 
 	if (!b)
 		return (0);
-	while (*b == '0' || *b == '1')
-	{
-		a <<= 1;
-		a += (*b - '0');
-		b++;
-	}
+	i = 0;
 
-	if (*b != '\0')
-		return (0);
-	return (a);
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base = 1; len >= 0; len--, base *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+
+		}
+		if (b[len] & 1)
+		{
+			i += base;
+		}
+	}
+	return (i);
 }
