68256716c55f9d3830eeb7b3a06513b9a33828a3
Avlnnwa <adukwuvictor15@gmail.com>
2023-09-30 16:15:09 -0700

    updated task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,28 +8,15 @@
 
 unsigned int binary_to_unit(const char *b)
 {
-	unsigned int i;
-	int len;
-	int base;
-
-	if (!b)
-		return (0);
-	i = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base = 1; len >= 0; len--, base *= 2)
-	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-
-		}
-		if (b[len] & 1)
-		{
-			i += base;
-		}
-	}
-	return (i);
+int a;
+unsigned int ans = 0;
+if (!b)
+return (0);
+for (a = 0; b[a]; a++)
+{
+if (b[a] < '0' || b[a] > '1')
+return (0);
+ans = 2 * ans + (b[a] - '0');
+}
+return (ans);
 }
