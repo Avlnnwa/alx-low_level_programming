c8602b0ec23dc1df880c15e4e5ece0fdf7f26644
Avlnnwa <adukwuvictor15@gmail.com>
2023-10-03 15:14:39 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,32 +1,39 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: The name of the file to append to.
- * @text_content: The text content to append.
- * Return: 1 on success, -1 on failure.
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ *
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ * or if it fails.
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
-int fd, write_r, count_let;
-if (!filename)
-return (-1);
-fd = open(filename, O_WRONLY | O_APPEND);
+	int fd;
+	int nletters;
+	int rwr;
 
-if (fd == -1)
-return (-1);
-if (text_content)
-{
-for (count_let = 0; text_content[count_let]; count_let++)
-write_r = write(fd, text_content, count_let);
-if (write_r == -1)
-{
-close(fd);
-return (-1);
-}
-}
-close(fd);
-return (1);
+	if (!filename)
+		return (-1);
 
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
+	}
+
+	close(fd);
+
+	return (1);
 }
